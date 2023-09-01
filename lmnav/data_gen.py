1532ca776ca9938bd1b46a0023715bb07d80991d
pranav-putta <pranav.putta22@gmail.com>
2023-09-01 11:03:21 -0400

    finish single threaded data_gen

--- lmnav/data_gen.py
+++ lmnav/data_gen.py
@@ -7,7 +7,10 @@ import numpy as np
 import habitat
 import habitat.gym
 from habitat_baselines.common.env_spec import EnvironmentSpec
-
+from habitat.utils.visualizations.utils import (
+    observations_to_image,
+    overlay_frame,
+)
 from habitat_baselines.rl.ddppo.ddp_utils import (
     EXIT,
     get_distrib_size,
@@ -22,6 +25,8 @@ import hydra
 from habitat_baselines.common.baseline_registry import baseline_registry
 
 import torch
+from habitat_baselines.utils.info_dict import extract_scalars_from_info
+
 
 from habitat_baselines.common.obs_transformers import (
     apply_obs_transforms_batch,
@@ -44,6 +49,7 @@ os.environ["MAGNUM_LOG"] = "quiet"
 os.environ["HABITAT_SIM_LOG"] = "quiet"
 
 os.chdir('/srv/flash1/pputta7/projects/lm-nav')
+
 
 def _init_envs(config=None, is_eval: bool = False):
     env_factory = hydra.utils.instantiate(config.habitat_baselines.vector_env_factory)
@@ -69,6 +75,7 @@ def _create_obs_transforms(config, env_spec):
     env_spec.observation_space = apply_obs_transforms_obs_space(
             env_spec.observation_space, obs_transforms
         )
+    return obs_transforms, env_spec
 
 def _setup_teacher(teacher_ckpt, obs_space, action_space):
     teacher = OldEAIPolicy.hardcoded(OldEAIPolicy, obs_space, action_space)
@@ -80,36 +87,66 @@ def _setup_teacher(teacher_ckpt, obs_space, action_space):
 
     teacher.load_state_dict(state_dict)
     return teacher
-    
 
-def collect_episodes(envs, teacher, obs_transform, N=None):
+
+def _construct_state_tensors(num_environments, device):
+    rnn_hx = torch.zeros((num_environments, 2, 512), device=device)
+    prev_actions = torch.zeros(num_environments, 1, device=device, dtype=torch.long)
+    not_done_masks = torch.ones(num_environments, 1, device=device, dtype=torch.bool)
+
+    return rnn_hx, prev_actions, not_done_masks 
+    
+    
+def collect_episodes(envs, teacher, obs_transform, device, deterministic=False, N=None):
+    device = torch.device(device)
+    num_envs = envs.num_envs
+    step = 0
+    dataset = []
+    episodes = [[] for _ in range(num_envs)]
+
+    rnn_hx, prev_actions, not_done_masks = _construct_state_tensors(num_envs, device)
+
+    teacher.to(device)
+    teacher.eval()
+    
     observations = envs.reset()
-    episodes = []
-    num_envs = len(envs)
 
-    while True:
-        batch = batch_obs([observations])
+    while (N is None) or (len(dataset) < N):
+        print(step)
+        # roll out a step
+        batch = batch_obs(observations, device)
         batch = apply_obs_transforms_batch(batch, obs_transform)
-        batch.apply(lambda x: x.to('cuda'))
-
+    
+        policy_result = teacher.act(batch,
+                                  rnn_hx,
+                                  prev_actions,
+                                  not_done_masks,
+                                  deterministic=deterministic)
         
-
-        if N is not None and len(episodes) == N:
-            return episodes
-            
- 
-def main():
-    config = habitat.get_config("config/imagenav_hm3d.yaml")
-    envs, env_spec = _init_envs(config)
-    obs_transform = _create_obs_transforms(config, env_spec)
+        prev_actions.copy_(policy_result.actions)
+        rnn_hx = policy_result.rnn_hidden_states
     
-    teacher_ckpt = "models/uLHP.300.pth"
-    teacher = _setup_teacher(teacher_ckpt, env_spec.observation_space, env_spec.action_space)
-
-    episodes = collect_episodes(envs, teacher, obs_transform)
+        step_data = [a.item() for a in policy_result.env_actions.cpu()]
+        outputs = envs.step(step_data)
+        next_observations, rewards_l, dones, infos = [list(x) for x in zip(*outputs)]
     
-
-   
-if __name__ == "__main__":
-    main()
+        # insert episode into list
+        for i, episode in enumerate(episodes):
+            episode.append((observations[i], rewards_l[i], infos[i]))
     
+        # check if any episodes finished and archive it into dataset
+        for i, done in enumerate(dones):
+            if done:
+                dataset.append(episodes[i])
+                episodes[i] = []
+    
+                # reset state tensors
+                rnn_hx[i] = torch.zeros(rnn_hx.shape[1:])
+                prev_actions[i] = torch.zeros(prev_actions.shape[1:])
+                not_done_masks[i] = torch.ones(not_done_masks.shape[1:])
+    
+        observations = next_observations
+        step += 1
+
+    return dataset
+
