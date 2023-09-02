e41f98c37438b690c5d73a891676df1a2926ea54
pranav-putta <pranav.putta22@gmail.com>
2023-09-02 16:06:13 -0400

    reorganize video llama into repo

---
+++ lmnav/tests/test_video_llama.py
@@ -0,0 +1,25 @@
+from lmnav.common.config import Config
+from lmnav.common.registry import registry
+
+#%%
+from lmnav.models import *
+from lmnav.processors import *
+
+import os
+
+from collections import namedtuple
+
+os.chdir('/srv/flash1/pputta7/projects/lm-nav')
+
+Args = namedtuple("Args", "cfg_path, model_type, gpu_id, options")
+args = Args("/srv/flash1/pputta7/projects/lm-nav/exp_configs/video_llama_eval_only_vl.yaml", "llama_v2", 0, [])
+
+cfg = Config(args)
+
+model_config = cfg.model_cfg
+model_cls = registry.get_model_class(model_config.arch)
+model = model_cls.from_config(model_config).to('cuda:{}'.format(args.gpu_id))
+model.eval()
+vis_processor_cfg = cfg.datasets_cfg.webvid.vis_processor.train
+vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
+
