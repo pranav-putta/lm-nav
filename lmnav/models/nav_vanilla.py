a8f01613a8b3f35748bc2d3dbdb09b0aa2a7774d
pranav-putta <pranav.putta22@gmail.com>
2023-10-06 23:34:40 -0400

    add action generator to vanilla transformer

--- lmnav/models/nav_vanilla.py
+++ lmnav/models/nav_vanilla.py
@@ -115,13 +115,16 @@ class NavVanillaTransformer(BaseModel):
         self.n_blocks = n_blocks
         self.drop_p = drop_p
 
-        self.max_t = (
-            max_trajectory_length
-            + (max_trajectory_length + 1) * self.vis_encoder.num_tokens
+        self.max_trajectory_length = max_trajectory_length
+        self.max_tokens = (
+            max_trajectory_length + (max_trajectory_length + 1) * self.tokens_per_img
         )  # action tokens + rgb tokens + goal token
 
         self.transformer = nn.Sequential(
-            *[Block(d_hidden, self.max_t, n_heads, drop_p) for _ in range(n_blocks)]
+            *[
+                Block(d_hidden, self.max_tokens, n_heads, drop_p)
+                for _ in range(n_blocks)
+            ]
         )
         self.vis_proj = nn.Linear(self.vis_encoder.hidden_size, self.d_hidden)
         self.action_embedding = nn.Embedding(4, self.d_hidden)
@@ -131,8 +134,18 @@ class NavVanillaTransformer(BaseModel):
     def hidden_size(self):
         return self.d_hidden
 
-    def embed_actions(self, actions_t):
-        raise NotImplementedError()
+    @property
+    def tokens_per_img(self):
+        return self.vis_encoder.num_tokens
+
+    def pad_sequences(self, seqs, dim):
+        p2d_partial = (0,) * ((len(seqs[0].shape) - dim - 1) * 2 + 1)
+        max_t = max([seq.shape[dim] for seq in seqs])
+
+        padded_seqs = [
+            F.pad(seq, p2d_partial + (max_t - seq.shape[dim],)) for seq in seqs
+        ]
+        return torch.stack(padded_seqs)
 
     def embed_visual(self, imgs):
         with self.maybe_autocast():
@@ -192,7 +205,7 @@ class NavVanillaTransformer(BaseModel):
             embd = torch.cat((goals_embd, sa_embds), dim=1)
 
             logits = self.transformer(embd)[
-                :, self.vis_encoder.num_tokens :: self.vis_encoder.num_tokens * 2
+                :, self.tokens_per_img :: self.tokens_per_img * 2
             ]
             probs = einops.rearrange(
                 F.softmax(self.action_head(logits), dim=-1), "b t h -> (b t) h"
@@ -203,3 +216,90 @@ class NavVanillaTransformer(BaseModel):
             loss = (F.cross_entropy(probs, tgts, reduction="none") * masks).mean()
 
         return NavVanillaTransformerOutput(loss=loss)
+
+    def action_generator(self, num_envs, deterministic=False, max_its=0):
+        """
+        action generator function, takes in the next rgb, goal, and action
+        """
+        T = self.max_trajectory_length
+
+        episodes = [[] for _ in range(num_envs)]
+
+        its = 0
+        while True:
+            (rgb_embds, goal_embds), dones = yield
+            if dones is None:
+                episodes.clear()
+
+            for i, episode in enumerate(episodes):
+                if dones[i]:
+                    episode.clear()
+
+                episode.append(
+                    {
+                        "rgb_embds": rgb_embds[i],
+                        "goal_embds": goal_embds[i],
+                        "action": 0,
+                    }
+                )
+
+            episodes = [e[-(T - 1) :] for e in episodes]
+
+            rgb_embds, goal_embds, actions = map(
+                lambda key: [[step[key] for step in episode] for episode in episodes],
+                ("rgb_embds", "goal_embds", "action"),
+            )
+            rgb_embds, goal_embds = map(
+                lambda seq: [torch.stack(t, dim=0) for t in seq],
+                (rgb_embds, goal_embds),
+            )
+            actions = [torch.tensor(t) for t in actions]
+            rgb_embds, goal_embds, actions_t = map(
+                lambda t: self.pad_sequences(t, dim=0), (rgb_embds, goal_embds, actions)
+            )
+            goal_embds = goal_embds[:, 0:1]
+            mask_t = torch.ones_like(actions_t, dtype=torch.bool).to(self.device)
+
+            lens = [len(e) for e in episodes]
+            max_len = max(lens)
+
+            actions_embd = einops.rearrange(
+                self.action_embedding(actions_t.long()), "b t h -> b t 1 h"
+            )
+            sa_embds = torch.cat((rgb_embds, actions_embd), dim=2)
+            sa_embds = einops.rearrange(sa_embds, "b t q h -> b (t q) h")
+            goals_embd = einops.rearrange(goal_embds, "b t q h -> b (t q) h")
+            embd = torch.cat((goals_embd, sa_embds), dim=1)
+
+            embd = embd.to(self.device)
+
+            outputs = self.transformer(embd)
+            act_pos_delta = [
+                (self.tokens_per_img + 1) * (max_len - l) + 2 for l in lens
+            ]
+            logits = self.transformer(embd)[
+                :, self.tokens_per_img :: self.tokens_per_img * 2
+            ]
+
+            # project onto the action space
+            actions = []
+            for i in range(len(episodes)):
+                act_logits = logits[i, -act_pos_delta[i]]
+                act_logits = F.softmax(act_logits)
+
+                if deterministic:
+                    action = act_logits.argmax().cpu().item()
+                else:
+                    action = torch.multinomial(act_logits, 1).cpu().item()
+                actions.append(action)
+
+                episodes[i][-1]["action"] = action
+
+            its += 1
+
+            if its == max_its:
+                embd.to("cpu")
+                episodes.clear()
+                torch.cuda.empty_cache()
+
+            yield actions
