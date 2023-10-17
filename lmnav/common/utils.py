c8ee91133fc7fa3cea4bc807318e9e3920c712b0
pranav-putta <pranav.putta22@gmail.com>
2023-10-17 01:42:02 -0400

    add GroupNorm to bf16 conversion

--- lmnav/common/utils.py
+++ lmnav/common/utils.py
@@ -476,7 +476,7 @@ def convert_weights_to_fp16(model: nn.Module):
     """Convert applicable model parameters to fp16"""
 
     def _convert_weights_to_fp16(l):
-        if isinstance(l, (nn.Conv1d, nn.Conv2d, nn.Linear)):
+        if isinstance(l, (nn.Conv1d, nn.Conv2d, nn.Linear, nn.GroupNorm)):
             l.weight.data = l.weight.data.to(torch.bfloat16)
             if l.bias is not None:
                 l.bias.data = l.bias.data.to(torch.bfloat16)
