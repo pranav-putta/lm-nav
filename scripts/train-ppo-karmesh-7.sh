ffcd8561691aa3c5ca376e3eb9356b157e7629ee
pranav-putta <pranav.putta22@gmail.com>
2023-12-21 00:39:15 -0500

    add 1e-5 experiment

---
+++ scripts/train-ppo-karmesh-7.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name train-ppo 
+#SBATCH --output logs/train-ppo-7.out
+#SBATCH --error logs/train-ppo-7.err
+#SBATCH --gpus a40:4
+#SBATCH --nodes 1
+#SBATCH --cpus-per-task 12
+#SBATCH --exclude "xaea-12,nestor,heistotron,gundam,baymax"
+#SBATCH --ntasks-per-node 4
+#SBATCH --partition cvmlp-lab
+#SBATCH --qos short
+#SBATCH --signal USR1@600
+
+MASTER_ADDR=$(srun --ntasks=1 hostname 2>&1 | tail -n1)
+export MASTER_ADDR
+
+zsh
+source ~/.zshrc
+mamba activate lmnav
+
+torchrun --standalone --nnodes=1 --nproc_per_node=4 lmnav/ppo_train.py train/nav_llama/1env_karmesh/rl/lora+clip+karmesh+7
+
+# description: normalized advantages, increased clip range, increased lr over experiment 3, long warmup critic over experiment 4
